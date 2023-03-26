6ad7618de52f7a3c42beb0c377041ea6c1acdb49
sungbum05 <sblim05@naver.com>
2023-03-27 02:22:30 +0900

    Player Attack

--- SkillContest/Assets/Scripts/Object/Player.cs
+++ SkillContest/Assets/Scripts/Object/Player.cs
@@ -10,10 +10,27 @@ public class Player : MonoBehaviour
     public Vector2 BorderX;
     public Vector2 BorderZ;
 
+    [Header("Attack - Rotation")]
+    public GameObject Gun;
+
+    public float RotationSpeed;
+    public float CurRot;
+    public float MaxRot, MinRot;
+
+    [Header("Attack - Fire")]
+    public GameObject BulletPrefab;
+    public float BulletForce;
+
+    public float MaxAttackDelay;
+    public float CurAttackDelay;
+
+    public Transform BulletSpawnPos;
+
     // Update is called once per frame
     void Update()
     {
         Move();
+        GunRotation();
     }
 
     void Move()
@@ -23,5 +40,47 @@ public class Player : MonoBehaviour
 
         this.transform.Translate(new Vector3(Horizontal, 0, Vertical) * MoveSpeed * Time.deltaTime);
         this.transform.position = new Vector3(Mathf.Clamp(transform.position.x, BorderX.y, BorderX.x), this.transform.position.y, Mathf.Clamp(transform.position.z, BorderZ.y, BorderZ.x));
+    }
+
+    void GunRotation()
+    {
+        if (CurAttackDelay > 0)
+            CurAttackDelay -= Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
+        {
+            CurRot = 0;
+
+            if (CurAttackDelay <= 0)
+                Fire();
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            CurRot = -1;
+
+            if (CurAttackDelay <= 0)
+                Fire();
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            CurRot = 1;
+
+            if (CurAttackDelay <= 0)
+                Fire();
+        }
+        else
+            CurRot = 0;
+
+        Gun.transform.Rotate(Vector3.up * CurRot * RotationSpeed * Time.deltaTime);
+    }
+
+    void Fire()
+    {
+        CurAttackDelay += MaxAttackDelay;
+
+        GameObject Bullet = Instantiate(BulletPrefab);
+        Bullet.transform.position = BulletSpawnPos.position;
+
+        Bullet.GetComponent<Rigidbody>().AddForce(Gun.transform.forward * BulletForce, ForceMode.Impulse);
     }
 }
